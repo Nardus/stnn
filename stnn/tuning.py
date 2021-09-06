685cc1203f17e65feee2d998f18de43090903cbf
Nardus Mollentze <nmollentze@gmail.com>
2021-09-06 13:10:49 +0100

    Switch to Trainable class

--- stnn/tuning.py
+++ stnn/tuning.py
@@ -8,6 +8,7 @@ import torch.nn.functional as functional
 import torch.optim as optim
 
 from ray import tune
+from copy import deepcopy
 
 from .utils import rmse
 from .datasets import load_data
@@ -45,202 +46,231 @@ default_config = {
 }
 
 
-def train_stnn_model(config, obs_path, relation_paths, train_inds, random_seed, 
-                     allow_gpu = False, validation_prop=0.2, checkpoint_dir=None):
-    """Train an stnn model using a dictionary of hyper-parameters
-    
-    Parameters
-    ----------
-    config : dict
-        A dictionary of model hyper-parameters
-    obs_path : str
-        Path to observation data.
-    relation_paths: list
-        List of paths to spatial relation matrices
-    train_inds : list
-        List of indices specifying the data rows to use for training and validation
-    random_seed: int
-        A random seed to pass to all utilized GPUs (not used when allow_gpu=False)
-    allow_gpu : bool
-        If true, all available GPUs will be used 
-    validation_prop : float, optional
-        Proportion of training data to hold back for model validation (i.e. evaluation of tuning)
-    checkpoint_dir : str
-        Directory for loading/saving model checkpoints
-    
-    Raises
-    ----------
-    ValueError : 
-        Input invalid (e.g. missing hyperparameter values or values outside their valid range)
-    """
-    # Device setup
-    if allow_gpu:
-        device = torch.device("cuda:0")
-        torch.cuda.manual_seed_all(random_seed)
-    else:
-        device = torch.device("cpu")
+class stnnTrainable(tune.Trainable):
+    def setup(self, config):
+        """
+        Trainable wrapper for the Spatio-Temporal Neural net model, compatible with ray.tune
+        
+        Parameters
+        ----------
+        config : dict
+            A dictionary of model hyper-parameters
+        obs_path : str
+            Path to observation data.
+        relation_paths: list
+            List of paths to spatial relation matrices
+        train_inds : list
+            List of indices specifying the data rows to use for training and validation
+        random_seed: int
+            A random seed to pass to all utilized GPUs (not used when allow_gpu=False)
+        allow_gpu : bool
+            If true, all available GPUs will be used 
+        validation_prop : float, optional
+            Proportion of training data to hold back for model validation (i.e. evaluation of tuning)
+        checkpoint_dir : str
+            Directory for loading/saving model checkpoints
 
-    # Load data
-    all_data, relations = load_data(obs_path, relation_paths)
+        Raises
+        ----------
+        ValueError : 
+            Input invalid (e.g. missing hyperparameter values or values outside their valid range)
+        """
+        # Data loading - performed only during initial setup 
+        # (and not when setup is called by reset() below)
+        # TODO: add to config dict: obs_path, relation_paths, train_inds, random_seed, allow_gpu, validation_prop=0.2
+        if not self.initialized:
+            # Device setup
+            if config["allow_gpu"]:
+                self.device = torch.device("cuda:0")
+                torch.cuda.manual_seed_all(config["random_seed"])
+            else:
+                self.device = torch.device("cpu")
 
-    # Data-related quantities / dimensions
-    nd = 1                  # dimension of input observations (1 = univariate time series)
-    nt = all_data.shape[0]  # number of observed timesteps
-    nx = all_data.shape[1]  # number of locations (columns)
-    nz = nd                 # dimension of observations in latent space
+            # Load data
+            all_data, relations = load_data(config["obs_path"], config["relation_paths"])
 
-    if config["obs_periodicity"] > 0:
-        # if periode < nt, latent factors will be initialized with a periodicity
-        periode = config["obs_periodicity"]
-    else:
-        periode = nt
+            # Data-related quantities / dimensions
+            self.nd = 1                  # dimension of input observations (1 = univariate time series)
+            self.nt = all_data.shape[0]  # number of observed timesteps
+            self.nx = all_data.shape[1]  # number of locations (columns)
+            self.nz = self.nd            # dimension of observations in latent space
 
-    # Split data
-    if validation_prop <=0 or validation_prop >= 1:
-        raise ValueError("validation_prop should be in (0, 1)")
-    
-    nt_train = len(train_inds)  # number of rows in training data
-    nt_actual = int(nt_train * (1 - validation_prop))
-    train_subset_inds = train_inds[:nt_actual]
-    val_subset_inds = train_inds[nt_actual:nt_train]
+            # Split data
+            if config["validation_prop"] <=0 or config["validation_prop"] >= 1:
+                raise ValueError("validation_prop should be in (0, 1)")
+            
+            train_inds = config["train_inds"]
+            nt_train = len(train_inds)  # number of rows in training data
+            self.nt_actual = int(nt_train * (1 - config["validation_prop"]))
+            train_subset_inds = train_inds[:self.nt_actual]
+            val_subset_inds = train_inds[self.nt_actual:nt_train]
 
-    train_data = all_data[train_subset_inds]
-    validation_data = all_data[val_subset_inds]
+            train_data = all_data[train_subset_inds]
+            validation_data = all_data[val_subset_inds]
 
-    train_data = train_data.to(device)
-    validation_data = validation_data.to(device)
-    relations = relations.to(device)
+            self.train_data = train_data.to(self.device)
+            self.validation_data = validation_data.to(self.device)
+            self.relations = relations.to(self.device)
 
-    # Set up model
-    model = SpatioTemporalNN(relations, nx, nt_actual, nd, nz, 
-                             mode = config["mode"], 
-                             nhid = config["nhid"], 
-                             nlayers = config["nlayers"],
-                             dropout_f = config["dropout_f"], 
-                             dropout_d = config["dropout_d"], 
-                             activation = config["activation"],
-                             periode = periode)
-    model = model.to(device)
+            self.initialized = True
 
-    # Train inputs
-    # - indices
-    t_idx = torch.arange(nt_actual, out = torch.LongTensor()).unsqueeze(1).expand(nt_actual, nx).contiguous()
-    x_idx = torch.arange(nx, out = torch.LongTensor()).expand_as(t_idx).contiguous()
+        # Always performed
+        self.config = config
 
-    # - dynamic
-    idx_dyn = torch.stack((t_idx[1:], x_idx[1:])).view(2, -1).to(device)
-    nex_dyn = idx_dyn.size(1)
+        # Set up model
+        if config["obs_periodicity"] > 0:
+            # if periode < nt, latent factors will be initialized with a periodicity
+            periode = config["obs_periodicity"]
+        else:
+            periode = self.nt
 
-    # - decoder
-    idx_dec = torch.stack((t_idx, x_idx)).view(2, -1).to(device)
-    nex_dec = idx_dec.size(1)
+        model = SpatioTemporalNN(relations, self.nx, self.nt_actual, self.nd, self.nz, 
+                                 mode = config["mode"], 
+                                 nhid = config["nhid"], 
+                                 nlayers = config["nlayers"],
+                                 dropout_f = config["dropout_f"], 
+                                 dropout_d = config["dropout_d"], 
+                                 activation = config["activation"],
+                                 periode = periode)
+        self.model = model.to(self.device)
 
-    # Optimizer
-    params = [{"params": model.factors_parameters(), "weight_decay": config["wd_z"]},
-              {"params": model.dynamic.parameters()},
-              {"params": model.decoder.parameters()}]
+        # Indices
+        # - all
+        t_idx = (torch.arange(self.nt_actual, out = torch.LongTensor())
+                 .unsqueeze(1)
+                 .expand(self.nt_actual, self.nx)
+                 .contiguous())
+        x_idx = (torch.arange(self.nx, out = torch.LongTensor())
+                 .expand_as(t_idx)
+                 .contiguous())
 
-    if config["mode"] in ("refine", "discover"):
-        params.append({"params": model.rel_parameters(), "weight_decay": 0.})
+        # - dynamic
+        self.idx_dyn = torch.stack((t_idx[1:], x_idx[1:])).view(2, -1).to(self.device)
+        self.nex_dyn = self.idx_dyn.size(1)
 
-    optimizer = optim.Adam(params, 
-                           lr=config["lr"], 
-                           betas=(config["beta1"], config["beta2"]), 
-                           eps=config["eps"], weight_decay=config["wd"])
+        # - decoder
+        self.idx_dec = torch.stack((t_idx, x_idx)).view(2, -1).to(self.device)
+        self.nex_dec = self.idx_dec.size(1)
 
-    if config["patience"] > 0:
-        lr_scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience = config["patience"])
+        # Optimizer
+        params = [{"params": self.model.factors_parameters(), "weight_decay": config["wd_z"]},
+                  {"params": self.model.dynamic.parameters()},
+                  {"params": self.model.decoder.parameters()}]
 
-    # Load from existing checkpoint:
-    if checkpoint_dir:
-        model_state, optimizer_state = torch.load(os.path.join(checkpoint_dir, "checkpoint"))
-        model.load_state_dict(model_state)
-        optimizer.load_state_dict(optimizer_state)
-    
-    # Training
-    lr = config["lr"]
+        if config["mode"] in ("refine", "discover"):
+            params.append({"params": self.model.rel_parameters(), "weight_decay": 0.})
 
-    for epoch in range(config["nepoch"]):
+        self.optimizer = optim.Adam(params, 
+                                    lr=config["lr"], 
+                                    betas=(config["beta1"], config["beta2"]), 
+                                    eps=config["eps"], weight_decay=config["wd"])
+
+        if config["patience"] > 0:
+            self.lr_scheduler = optim.lr_scheduler.ReduceLROnPlateau(self.optimizer, patience = config["patience"])
+        
+
+    def step(self):
+        """Perform a single iteration (epoch) of training"""
         # ------------------------ Train ------------------------
-        model.train()
+        self.model.train()
         
         # --- decoder ---
-        idx_perm = torch.randperm(nex_dec).to(device)
-        batches = idx_perm.split(config["batch_size"])
+        idx_perm = torch.randperm(self.nex_dec).to(self.device)
+        batches = idx_perm.split(self.config["batch_size"])
 
-        for i, batch in enumerate(batches):
-            optimizer.zero_grad()
+        for batch in batches:
+            self.optimizer.zero_grad()
 
             # data
-            input_t = idx_dec[0][batch]
-            input_x = idx_dec[1][batch]
-            x_target = train_data[input_t, input_x]
+            input_t = self.idx_dec[0][batch]
+            input_x = self.idx_dec[1][batch]
+            x_target = self.train_data[input_t, input_x]
 
             # closure
-            x_rec = model.dec_closure(input_t, input_x)
+            x_rec = self.model.dec_closure(input_t, input_x)
             mse_dec = functional.mse_loss(x_rec, x_target)
 
             # backward
             mse_dec.backward()
 
             # step
-            optimizer.step()
+            self.optimizer.step()
 
         # --- dynamic ---
-        idx_perm = torch.randperm(nex_dyn).to(device)
-        batches = idx_perm.split(config["batch_size"])
+        idx_perm = torch.randperm(self.nex_dyn).to(self.device)
+        batches = idx_perm.split(self.config["batch_size"])
 
-        for i, batch in enumerate(batches):
-            optimizer.zero_grad()
+        for batch in batches:
+            self.optimizer.zero_grad()
 
             # data
-            input_t = idx_dyn[0][batch]
-            input_x = idx_dyn[1][batch]
+            input_t = self.idx_dyn[0][batch]
+            input_x = self.idx_dyn[1][batch]
 
             # closure
-            z_inf = model.factors[input_t, input_x]
-            z_pred = model.dyn_closure(input_t - 1, input_x)
+            z_inf = self.model.factors[input_t, input_x]
+            z_pred = self.model.dyn_closure(input_t - 1, input_x)
 
             # loss
             mse_dyn = z_pred.sub(z_inf).pow(2).mean()
-            loss_dyn = mse_dyn * config["lambd"]
+            loss_dyn = mse_dyn * self.config["lambd"]
 
-            if config["l2_z"] > 0:
-                loss_dyn += config["l2_z"] * model.factors[input_t - 1, input_x].sub(model.factors[input_t, input_x]).pow(2).mean()
+            if self.config["l2_z"] > 0:
+                temp_factors = self.model.factors[input_t - 1, input_x]
+                temp_factors = temp_factors.sub(self.model.factors[input_t, input_x])
+                temp_factors = temp_factors.pow(2).mean()
+                loss_dyn += self.config["l2_z"] * temp_factors
 
-            if config["mode"] in("refine", "discover") and config["l1_rel"] > 0:
-                loss_dyn += config["l1_rel"] * model.get_relations().abs().mean()
+            if self.config["mode"] in("refine", "discover") and self.config["l1_rel"] > 0:
+                loss_dyn += self.config["l1_rel"] * self.model.get_relations().abs().mean()
 
             # backward
             loss_dyn.backward()
 
             # step
-            optimizer.step()
+            self.optimizer.step()
 
         # ------------------------ Validate ------------------------
-        model.eval()
+        self.model.eval()
 
         with torch.no_grad():
-            x_pred, _ = model.generate(validation_data.shape[0])
-            score = rmse(x_pred, validation_data)
-        
-        # checkpoint / report
-        with tune.checkpoint_dir(epoch) as checkpoint_dir:
-            path = os.path.join(checkpoint_dir, "checkpoint")
-            torch.save((model.state_dict(), optimizer.state_dict()), path)
+            x_pred, _ = self.model.generate(self.validation_data.shape[0])
+            score = rmse(x_pred, self.validation_data)
 
-        tune.report(validation_rmse = score, learn_rate = lr)
+        tune.report()
 
-        # schedule lr
-        if config["patience"] > 0 and score < 1:
-            lr_scheduler.step(score)
+        # schedule lr reduction
+        if self.config["patience"] > 0 and score < 1:
+            self.lr_scheduler.step(score)
 
-        lr = optimizer.param_groups[0]["lr"]
-
-        if lr <= 1e-5:
-            break
+        # report 
+        lr = self.optimizer.param_groups[0]["lr"]
+        return {"validation_rmse": score, "learn_rate": lr}
 
 
+    # Other methods
+    def save_checkpoint(self, checkpoint_dir):
+        """Save a checkpoint"""
+        path = os.path.join(checkpoint_dir, "checkpoint")
+        torch.save((self.model.state_dict(), self.optimizer.state_dict()), path)
+        return path
+
+
+    def load_checkpoint(self, checkpoint):
+        """Load a previously-saved checkpoint"""
+        model_state, optimizer_state = torch.load(checkpoint)
+        self.model.load_state_dict(model_state)
+        self.optimizer.load_state_dict(optimizer_state)
+
+
+    def reset_config(self, new_config):
+        """Reset this object with a new config (allows re-use without loading data again)"""
+        self.setup(deepcopy(new_config))
+        return self.config == new_config
+
+
+
+# Additional utility functions
 def restore_model(model_settings, config, allow_gpu=False):
         """
         Restore a checkpointed model after tuning
@@ -299,7 +329,7 @@ def restore_model(model_settings, config, allow_gpu=False):
         model = model.to(device)
 
         checkpoint_path = os.path.join(model_settings["checkpoint_path"], "checkpoint")
-        model_state, optimizer_state = torch.load(checkpoint_path)
+        model_state, _ = torch.load(checkpoint_path)
         model.load_state_dict(model_state)
 
         return all_data, relations, model
